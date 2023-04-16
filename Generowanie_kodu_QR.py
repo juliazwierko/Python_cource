08ccfe1228e974a89553a550fb5c61c0b4d8299e
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-04-16 20:45:12 +0200

    Update Generowanie_kodu_QR.py

--- Generowanie_kodu_QR.py
+++ Generowanie_kodu_QR.py
@@ -2,7 +2,7 @@ import segno
 
 qrcode = segno.make_qr("siema to ja https://github.com/juliazwierko")
 # цвет - #2980B9, граница - 4, масштабирование - в 5 раз
-qrcode.save("metanit_qr.png", dark="#2980B9", border=4, scale=5)
+qrcode.save("bluhhh_qr.png", dark="#2980B9", border=4, scale=5)
 
 
 
